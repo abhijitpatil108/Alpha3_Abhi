c8881bce979ce52a3e65423c3024e1e4628a943f
abhijitpatil108 <abhijitpatil19@gmail.com>
2022-01-27 01:35:41 +0530

    Write a program to check if the given string is in lower case

---
+++ condistional_operators/prog1.py
@@ -0,0 +1,11 @@
+# """Write a program to check if the given string is in lower case?"""
+
+char = input("enter a character: ")
+
+# if "a" <= char <= "z":
+#     print(f"{char} is lowercase character")
+
+if char.islower():
+      print(f"{char} is lowercase character")
+
+
